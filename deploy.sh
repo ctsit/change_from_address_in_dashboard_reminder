a3c86af94b51769e161ee2ce126412e738061a16
Kyle Chesney <kyle.2493@gmail.com>
2020-05-05 17:52:56 -0400

    update deploy.sh to support versioned patch files add 9.9.0.patch

--- deploy.sh
+++ deploy.sh
@@ -18,5 +18,21 @@ export REDCAP_VERSION=$2
 # determine the directory where this script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+PATCH_VERSIONS=$(mktemp)
+$(ls *.patch | grep -v "sql" | sed "s/\.patch//;" > $PATCH_VERSIONS)
+if [[ $(cat $PATCH_VERSIONS | grep "$REDCAP_VERSION") ]]; then
+    REDCAP_PATCH_VERSION=$REDCAP_VERSION
+else
+    echo "${REDCAP_VERSION}" >> "$PATCH_VERSIONS"
+    if [[ "$(cat $PATCH_VERSIONS | sort -V | head -n 1)" == "$REDCAP_VERSION" ]]; then
+        # provided version is earlier than any updated version, use base patch.patch file
+        REDCAP_PATCH_VERSION="patch"
+    else
+        # use closest lower version patch file
+        REDCAP_PATCH_VERSION=$(cat $PATCH_VERSIONS | sort -V | grep "$REDCAP_VERSION" -B 1 | head -n1)
+    fi
+fi
+rm $PATCH_VERSIONS
+
 cd $REDCAP_ROOT/redcap_v$REDCAP_VERSION/Classes/
-patch -p1 < $DIR/patch.patch
+patch -p1 --dry-run < $DIR/${REDCAP_PATCH_VERSION}.patch
